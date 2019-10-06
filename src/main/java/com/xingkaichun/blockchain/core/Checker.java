f93e64797821921e02b053a2cef9b2486b291a16
xing kaichun <409060350@qq.com>
2019-10-06 14:22:26 +0800

    update

--- src/main/java/com/xingkaichun/blockchain/core/Checker.java
+++ src/main/java/com/xingkaichun/blockchain/core/Checker.java
@@ -2,6 +2,7 @@ package com.xingkaichun.blockchain.core;
 
 
 import com.xingkaichun.blockchain.core.model.Block;
+import com.xingkaichun.blockchain.core.model.transaction.Transaction;
 
 /**
  * 区块校验者
@@ -11,4 +12,10 @@ public interface Checker {
      * 检测区块
      */
     void checkBlock(BlockChainCore blockChainCore, Block block) throws Exception;
+
+    /**
+     * 校验交易的合法性
+     */
+    void checkTransaction(BlockChainCore blockChainCore, Transaction transaction) throws Exception;
+
 }
