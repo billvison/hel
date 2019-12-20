766f3f122f0896d3bf9e9015fd9d60449454a8cb
xingkaichun <xingkaichun@ceair.com>
2019-12-20 16:17:40 +0800

    update20191220006

--- src/main/java/com/xingkaichun/blockchain/core/utils/BlockUtils.java
+++ src/main/java/com/xingkaichun/blockchain/core/utils/BlockUtils.java
@@ -8,9 +8,4 @@ public class BlockUtils {
     public static String calculateHash(Block block) {
         return CipherUtil.applySha256(block.getPreviousHash() + block.getNonce() + block.getMerkleRoot());
     }
-
-    public static boolean checkHash(Block block) {
-        String hash = calculateHash(block);
-        return block.getHash().equals(hash);
-    }
 }
