2c9c746d8fbe63e1d4dd8aa35e84be77342d70c1
邢开春 <xingkaichun@ceair.com>
2019-12-31 19:49:49 +0800

    update2019123106

--- src/main/java/com/xingkaichun/blockchain/core/BlockChainCore.java
+++ src/main/java/com/xingkaichun/blockchain/core/BlockChainCore.java
@@ -1,7 +1,10 @@
 package com.xingkaichun.blockchain.core;
 
+import com.xingkaichun.blockchain.core.miner.MineAward;
+import com.xingkaichun.blockchain.core.miner.MineDifficulty;
 import com.xingkaichun.blockchain.core.miner.Miner;
 import com.xingkaichun.blockchain.core.miner.ForMinerTransactionDataBase;
+import com.xingkaichun.blockchain.core.model.key.PublicKeyString;
 
 public class BlockChainCore {
 
@@ -9,6 +12,8 @@ public class BlockChainCore {
     private Miner miner;
     private ForMinerTransactionDataBase forMinerTransactionDataBase;
 
+    public BlockChainCore(ForMinerTransactionDataBase forMinerTransactionDataBase, MineDifficulty mineDifficulty, MineAward mineAward, PublicKeyString minerPublicKey) {
 
 
+    }
 }
