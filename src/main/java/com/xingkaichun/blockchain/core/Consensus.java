3818ed468403f6ff27da131bc093a3f517edeea9
邢开春 <xingkaichun@ceair.com>
2020-01-07 19:36:52 +0800

    update20200107107

--- src/main/java/com/xingkaichun/blockchain/core/Consensus.java
+++ src/main/java/com/xingkaichun/blockchain/core/Consensus.java
@@ -7,5 +7,10 @@ import com.xingkaichun.blockchain.core.model.Block;
  */
 public interface Consensus {
 
+    /**
+     * 这个区块写入的nonce达成共识了吗？
+     * @param blockChainDataBase 区块链
+     * @param block 需要被验证nonce是否已经达成了共识的区块
+     */
     boolean isReachConsensus(BlockChainDataBase blockChainDataBase, Block block);
 }
