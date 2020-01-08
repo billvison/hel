6b8a26dfcd16dfc3ae2856ee7999b28403d126dc
邢开春 <xingkaichun@ceair.com>
2020-01-08 11:39:43 +0800

    update2020010801

--- src/main/java/com/xingkaichun/blockchain/core/Incentive.java
+++ src/main/java/com/xingkaichun/blockchain/core/Incentive.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 public interface Incentive {
 
     /**
-     * 挖矿的奖励
+     * 挖矿者的最大挖矿激励，挖矿者获取的到的挖矿激励应当不大于这个值。
      * @param blockChainDataBase 区块链
      * @param block 待挖矿的区块
      * @return
