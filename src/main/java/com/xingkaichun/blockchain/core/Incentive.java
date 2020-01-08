78fc8e110ef03c798d7d3f41865c1963240cb46a
邢开春 <xingkaichun@ceair.com>
2020-01-08 10:23:56 +0800

    update2020010802

--- src/main/java/com/xingkaichun/blockchain/core/Incentive.java
+++ src/main/java/com/xingkaichun/blockchain/core/Incentive.java
@@ -1,7 +1,17 @@
 package com.xingkaichun.blockchain.core;
 
+import com.xingkaichun.blockchain.core.model.Block;
+import com.xingkaichun.blockchain.core.model.transaction.Transaction;
+
 /**
  * 挖矿激励
  */
 public interface Incentive {
+
+    /**
+     * 生成激励交易
+     * @param blockChainDataBase 区块链
+     * @param block 需要产生激励交易的区块
+     */
+    Transaction incentiveTransaction(BlockChainDataBase blockChainDataBase, Block block);
 }
