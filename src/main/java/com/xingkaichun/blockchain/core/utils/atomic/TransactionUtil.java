ac1bf681585668a1cbdc9428b441c53e222e8dfa
xing kaichun <409060350@qq.com>
2020-02-20 11:17:38 +0800

    2020022006

--- src/main/java/com/xingkaichun/blockchain/core/utils/atomic/TransactionUtil.java
+++ src/main/java/com/xingkaichun/blockchain/core/utils/atomic/TransactionUtil.java
@@ -1,5 +1,7 @@
 package com.xingkaichun.blockchain.core.utils.atomic;
 
+import com.xingkaichun.blockchain.core.dto.TransactionDTO;
+import com.xingkaichun.blockchain.core.dto.TransactionOutputDTO;
 import com.xingkaichun.blockchain.core.model.key.PrivateKeyString;
 import com.xingkaichun.blockchain.core.model.key.PublicKeyString;
 import com.xingkaichun.blockchain.core.model.transaction.Transaction;
@@ -59,8 +61,39 @@ public class TransactionUtil {
      * @return
      */
     public static String signatureData(Transaction transaction) throws Exception {
-        String data = transaction.getTimestamp() + transaction.getTransactionUUID() + getSenderPublicKeyString(transaction)
-                        + getInputUtxoIds(transaction) + getOutpuUtxoIds(transaction);
+        String data = signatureData(transaction.getTimestamp(),transaction.getTransactionUUID(),getInputUtxoIds(transaction),getOutpuUtxoIds(transaction));
+        String sha256Data = CipherUtil.applySha256(data);
+        return sha256Data;
+    }
+
+    /**
+     * 用于签名的数据数据
+     * @return
+     */
+    public static String signatureData(TransactionDTO transactionDTO) throws Exception {
+        String data = signatureData(transactionDTO.getTimestamp(),transactionDTO.getTransactionUUID(),getInputUtxoIds(transactionDTO),getOutpuUtxoIds(transactionDTO));
+        String sha256Data = CipherUtil.applySha256(data);
+        return sha256Data;
+    }
+
+    /**
+     * 用于签名的数据数据
+     * @return
+     */
+    public static String signatureData(long timestamp,String transactionUUID,List<String> inputUtxoUuidList,List<String> outputUtxoUuidList) throws Exception {
+        String inputs = "";
+        if(inputUtxoUuidList != null){
+            for(String input:inputUtxoUuidList){
+                inputs += input;
+            }
+        }
+        String outputs = "";
+        if(outputUtxoUuidList != null){
+            for(String output:outputUtxoUuidList){
+                outputs += output;
+            }
+        }
+        String data = timestamp + transactionUUID + inputs + outputs;
         String sha256Data = CipherUtil.applySha256(data);
         return sha256Data;
     }
@@ -80,6 +113,15 @@ public class TransactionUtil {
     public static String signature(Transaction transaction, PrivateKeyString privateKeyString) throws Exception {
         PrivateKey privateKey = KeyUtil.convertPrivateKeyStringToPrivateKey(privateKeyString);
         byte[] bytesSignature = CipherUtil.applyECDSASig(privateKey,signatureData(transaction));
+        String strSignature = Base64.getEncoder().encodeToString(bytesSignature);
+        return strSignature;
+    }
+    /**
+     * 交易签名
+     */
+    public static String signature(TransactionDTO transactionDTO, PrivateKeyString privateKeyString) throws Exception {
+        PrivateKey privateKey = KeyUtil.convertPrivateKeyStringToPrivateKey(privateKeyString);
+        byte[] bytesSignature = CipherUtil.applyECDSASig(privateKey,signatureData(transactionDTO));
         String strSignature = Base64.getEncoder().encodeToString(bytesSignature);
         return strSignature;
     }
@@ -106,9 +148,28 @@ public class TransactionUtil {
 
     public static List<String> getOutpuUtxoIds(Transaction transaction){
         List<String> ids = new ArrayList<>();
-        if(transaction.getInputs()==null||ids.size()==0){return ids;}
-        for(TransactionOutput transactionOutput:transaction.getOutputs()){
+        ArrayList<TransactionOutput> outputs = transaction.getOutputs();
+        if(outputs==null || outputs.size()==0){
+            return ids;
+        }
+        for(TransactionOutput transactionOutput:outputs){
             ids.add(transactionOutput.getTransactionOutputUUID());
+        }
+        return ids;
+    }
+
+    public static List<String> getInputUtxoIds(TransactionDTO transactionDTO){
+        return transactionDTO.getInputs();
+    }
+
+    public static List<String> getOutpuUtxoIds(TransactionDTO transactionDTO){
+        List<String> ids = new ArrayList<>();
+        ArrayList<TransactionOutputDTO> output = transactionDTO.getOutputs();
+        if(output==null || output.size()==0){
+            return ids;
+        }
+        for(TransactionOutputDTO transactionOutputDTO:output){
+            ids.add(transactionOutputDTO.getTransactionOutputUUID());
         }
         return ids;
     }
--- src/test/java/com/xingkaichun/blockchain/core/TranslateTest.java
+++ src/test/java/com/xingkaichun/blockchain/core/TranslateTest.java
@@ -1,10 +1,18 @@
 package com.xingkaichun.blockchain.core;
 
 import com.google.gson.Gson;
+import com.xingkaichun.blockchain.core.dto.TransactionDTO;
+import com.xingkaichun.blockchain.core.dto.TransactionOutputDTO;
 import com.xingkaichun.blockchain.core.impl.*;
 import com.xingkaichun.blockchain.core.model.Block;
+import com.xingkaichun.blockchain.core.model.key.PrivateKeyString;
 import com.xingkaichun.blockchain.core.model.key.PublicKeyString;
-import com.xingkaichun.blockchain.core.model.transaction.Transaction;
+import com.xingkaichun.blockchain.core.model.transaction.TransactionType;
+import com.xingkaichun.blockchain.core.utils.atomic.TransactionUtil;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class TranslateTest {
 
@@ -29,11 +37,19 @@ public class TranslateTest {
 
         BlockChainCore blockChainCore = new BlockChainCore(miner,synchronizer);
 
-        Transaction transaction = new Transaction();
-/*        transaction.setTimestamp();
-        transaction.setTransactionUUID();*/
-
-        //minerTransactionDtoDataBase.insertTransaction(transaction);
+        TransactionDTO transactionDTO = new TransactionDTO();
+        transactionDTO.setTimestamp(System.currentTimeMillis());
+        transactionDTO.setTransactionUUID(UUID.randomUUID().toString());
+        transactionDTO.setTransactionType(TransactionType.MINER);
+        ArrayList<String> inputs = new ArrayList<>();
+        transactionDTO.setInputs(inputs);
+        ArrayList<TransactionOutputDTO> outputs = new ArrayList<>();
+        TransactionOutputDTO transactionOutputDTO = new TransactionOutputDTO();
+        transactionOutputDTO.setTransactionOutputUUID(UUID.randomUUID().toString());
+        transactionOutputDTO.setReciepient(new PublicKeyString("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+        transactionOutputDTO.setValue(new BigDecimal("20"));
+        transactionDTO.setOutputs(outputs);
+        transactionDTO.setSignature(TransactionUtil.signature(transactionDTO,new PrivateKeyString("MIGNAgEAMBAGByqGSM49AgEGBSuBBAAKBHYwdAIBAQQgilXZ39cKVHuzFNjUaZSIPfBh8qWxgHLjKupFPuAezymgBwYFK4EEAAqhRANCAASvClummn+R3t2Ls1dVzHJM93htymb1lACtvIxXbi95Xh/MOcayk808aH3nfQaeE/JwaIQoYsM1yFOLJDn7NZOU")));
 
         int height = blockChainDataBase.findTailBlock().getHeight();
         for(int i=1;i<=height;i++){
