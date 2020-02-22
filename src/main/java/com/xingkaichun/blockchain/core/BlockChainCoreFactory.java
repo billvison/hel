2c4a4538b2a671ec9247f2172b57862270e9f29e
xing kaichun <409060350@qq.com>
2020-02-22 10:47:12 +0800

    2020022204

--- src/main/java/com/xingkaichun/blockchain/core/BlockChainCoreFactory.java
+++ src/main/java/com/xingkaichun/blockchain/core/BlockChainCoreFactory.java
@@ -21,16 +21,13 @@ public class BlockChainCoreFactory {
         Miner miner = new MinerDefaultImpl(blockChainDataBase, minerTransactionDtoDataBase,minerPublicKey);
 
         SynchronizerDataBase synchronizerDataBase = new SynchronizerDataBaseDefaultImpl(new File(blockchainPath,"Synchronizer").getAbsolutePath(),"otherNodeBlock.data",transactionDataBase);
-        BlockChainDataBase blockChainDataBaseDuplicate = new BlockChainDataBaseDefaultImpl(new File(blockchainPath,"BlockChainDataBaseDuplicate").getAbsolutePath(),incentive,consensus);
-        Synchronizer synchronizer = new SynchronizerDefaultImpl(blockChainDataBase,blockChainDataBaseDuplicate, synchronizerDataBase);
+        BlockChainDataBase temporaryBlockChainDataBase = new BlockChainDataBaseDefaultImpl(new File(blockchainPath,"BlockChainDataBaseDuplicate").getAbsolutePath(),incentive,consensus);
+        Synchronizer synchronizer = new SynchronizerDefaultImpl(blockChainDataBase,temporaryBlockChainDataBase, synchronizerDataBase);
 
         BlockChainCore blockChainCore = new BlockChainCoreImpl();
         blockChainCore.setMiner(miner);
         blockChainCore.setSynchronizer(synchronizer);
         blockChainCore.setBlockChainDataBase(blockChainDataBase);
-        blockChainCore.setConsensus(consensus);
-        blockChainCore.setIncentive(incentive);
-        blockChainCore.setMinerTransactionDtoDataBase(minerTransactionDtoDataBase);
         return blockChainCore;
     }
 }
