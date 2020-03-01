4edc8084d4b7570920688abc68b36d9b560072b3
xing kaichun <409060350@qq.com>
2020-03-01 12:24:09 +0800

    2020030102

--- src/test/java/com/xingkaichun/blockchain/core/MineTest.java
+++ src/test/java/com/xingkaichun/blockchain/core/MineTest.java
@@ -8,9 +8,9 @@ public class MineTest {
     @org.junit.Test
     public void test() throws Exception {
         String blockchainPath = "D:\\logs\\hellowordblockchain\\" ;
-        String minerStringPublicKey = "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAErwpbppp/kd7di7NXVcxyTPd4bcpm9ZQArbyMV24veV4fzDnGspPNPGh9530GnhPycGiEKGLDNchTiyQ5+zWTlA==" ;
+        String createBlockChainCore = "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAErwpbppp/kd7di7NXVcxyTPd4bcpm9ZQArbyMV24veV4fzDnGspPNPGh9530GnhPycGiEKGLDNchTiyQ5+zWTlA==" ;
 
-        BlockChainCore blockChainCore = new BlockChainCoreFactory().createBlockChainCore(blockchainPath,minerStringPublicKey);
+        BlockChainCore blockChainCore = new BlockChainCoreFactory().createBlockChainCore(blockchainPath,createBlockChainCore);
         blockChainCore.start();
 
         BlockChainDataBase blockChainDataBase = blockChainCore.getBlockChainDataBase();
--- src/test/java/com/xingkaichun/blockchain/core/TranslateTest.java
+++ src/test/java/com/xingkaichun/blockchain/core/TranslateTest.java
@@ -18,9 +18,9 @@ public class TranslateTest {
     @org.junit.Test
     public void test() throws Exception {
         String blockchainPath = "D:\\logs\\hellowordblockchain\\" ;
-        String minerStringPublicKey = "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAErwpbppp/kd7di7NXVcxyTPd4bcpm9ZQArbyMV24veV4fzDnGspPNPGh9530GnhPycGiEKGLDNchTiyQ5+zWTlA==" ;
+        String createBlockChainCore = "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAErwpbppp/kd7di7NXVcxyTPd4bcpm9ZQArbyMV24veV4fzDnGspPNPGh9530GnhPycGiEKGLDNchTiyQ5+zWTlA==" ;
 
-        BlockChainCore blockChainCore = new BlockChainCoreFactory().createBlockChainCore(blockchainPath,minerStringPublicKey);
+        BlockChainCore blockChainCore = new BlockChainCoreFactory().createBlockChainCore(blockchainPath,createBlockChainCore);
         blockChainCore.start();
 
 
