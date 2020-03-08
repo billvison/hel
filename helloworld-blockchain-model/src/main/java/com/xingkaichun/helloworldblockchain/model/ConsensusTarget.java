b4fc1e42ed424e2a3bfd4cff0e431f46d07aa77c
xing kaichun <409060350@qq.com>
2020-03-08 15:34:31 +0800

    添加序列化标识

--- helloworld-blockchain-model/src/main/java/com/xingkaichun/helloworldblockchain/model/ConsensusTarget.java
+++ helloworld-blockchain-model/src/main/java/com/xingkaichun/helloworldblockchain/model/ConsensusTarget.java
@@ -2,11 +2,13 @@ package com.xingkaichun.helloworldblockchain.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 挖矿共识目标
  */
 @Data
-public class ConsensusTarget {
+public class ConsensusTarget implements Serializable {
 
     private String value;
 }
