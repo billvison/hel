adb803619683ed00f5c57ae077ee86960b502864
邢开春 <xingkaichun@ceair.com>
2020-03-11 23:43:51 +0800

    update分叉同步

--- helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/impl/SynchronizerDefaultImpl.java
+++ helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/impl/SynchronizerDefaultImpl.java
@@ -99,8 +99,8 @@ public class SynchronizerDefaultImpl extends Synchronizer {
                 blockDTO = synchronizerDataBase.getBlockDto(availableSynchronizeNodeId,minBlockHeight);
             }
         }
-        synchronizerDataBase.clear(availableSynchronizeNodeId);
         promoteTargetBlockChainDataBase(targetBlockChainDataBase, temporaryBlockChainDataBase);
+        synchronizerDataBase.clear(availableSynchronizeNodeId);
     }
 
     /**
