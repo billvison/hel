29d3de5cc7703f7ae46090d3dbc293bd60216f37
邢开春 <xingkaichun@ceair.com>
2020-05-04 10:12:28 +0800

    优化参数

--- helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/utils/BlockChainCoreConstant.java
+++ helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/utils/BlockChainCoreConstant.java
@@ -100,11 +100,11 @@ public class BlockChainCoreConstant {
      */
     public static class TransactionConstant{
         //交易金额的小数点保留位数限制
-        public final static int TRANSACTION_AMOUNT_MAX_DECIMAL_PLACES = 8;
+        public final static int TRANSACTION_AMOUNT_MAX_DECIMAL_PLACES = 0;
         //最大交易金额
         public final static BigDecimal TRANSACTION_MAX_AMOUNT = new BigDecimal("1000000000000000000000000");
         //最小交易金额
-        public final static BigDecimal TRANSACTION_MIN_AMOUNT = new BigDecimal("0.00000001");
+        public final static BigDecimal TRANSACTION_MIN_AMOUNT = new BigDecimal("1");
         //最小交易手续费
         public final static BigDecimal MIN_TRANSACTION_FEE = new BigDecimal("1");
     }
