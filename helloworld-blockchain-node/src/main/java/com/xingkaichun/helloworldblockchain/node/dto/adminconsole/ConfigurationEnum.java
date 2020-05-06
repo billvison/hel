53c258084fc1c21304f8e87e3d4b966bb2f8b3f8
邢开春 <xingkaichun@ceair.com>
2020-05-06 15:48:11 +0800

    优化

--- helloworld-blockchain-node/src/main/java/com/xingkaichun/helloworldblockchain/node/dto/adminconsole/ConfigurationEnum.java
+++ helloworld-blockchain-node/src/main/java/com/xingkaichun/helloworldblockchain/node/dto/adminconsole/ConfigurationEnum.java
@@ -11,7 +11,8 @@ public enum ConfigurationEnum {
     AUTO_SEARCH_NODE("true","是否允许节点自动在区块链网络中搜索节点。"),
     FORK_BLOCK_SIZE("100","两个区块链有分叉时，区块差异个数大于这个值，则真的分叉了。"),
     NODE_ERROR_CONNECTION_TIMES_REMOVE_THRESHOLD("10","本地节点发现某一个节点错误次数过多，则删除该节点。这个阈值配置。"),
-    NODE_SEARCH_NEW_NODE_TIME_INTERVAL(String.valueOf(6*60*60*1000),"在区块链网络中自动搜寻新的节点的间隔时间。"),
+    //TODO 更新节点 更新节点的高度 时间太低，如果是内网会导致，外网连不到内网，所以外网节点不能主动将自己的高度通知到内网节点
+    NODE_SEARCH_NEW_NODE_TIME_INTERVAL(String.valueOf(2*60*1000),"在区块链网络中自动搜寻新的节点的间隔时间。"),
     SEARCH_NEW_BLOCKS_TIME_INTERVAL(String.valueOf(1*60*1000),"在区块链网络中自动搜寻新的区块的间隔时间。"),
     CHECK_LOCAL_BLOCKCHAIN_HEIGHT_IS_HIGH_TIME_INTERVAL(String.valueOf(1*60*1000),"检查自己的区块链高度在区块链网络中是否是最大的高度的时间间隔。"),
     ADD_SEED_NODE_TO_LOCAL_BLOCKCHAIN_TIME_INTERVAL(String.valueOf(1*60*60*1000),"定时将种子节点加入本地区块链网络的时间间隔。");
