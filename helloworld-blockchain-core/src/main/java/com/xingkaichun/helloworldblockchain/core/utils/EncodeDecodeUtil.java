cd42d3c8b1619d4635717d7313c0e7a87541f382
邢开春 <xingkaichun@qq.com>
2020-05-26 11:29:24 +0800

    代码优化

--- helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/utils/EncodeDecodeUtil.java
+++ helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/utils/EncodeDecodeUtil.java
@@ -8,7 +8,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.List;
 
 /**
  * EncodeDecode工具类
@@ -19,8 +18,7 @@ public class EncodeDecodeUtil {
 
     public static byte[] encode(Transaction transaction) throws Exception {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = null;
-        objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
         objectOutputStream.writeObject(transaction);
         byte[] bytesTransaction = byteArrayOutputStream.toByteArray();
         return bytesTransaction;
@@ -28,16 +26,14 @@ public class EncodeDecodeUtil {
 
     public static Transaction decodeToTransaction(byte[] bytesTransaction) throws Exception {
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytesTransaction);
-        ObjectInputStream objectInputStream = null;
-        objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
         Transaction transaction = (Transaction) objectInputStream.readObject();
         return transaction;
     }
 
     public static byte[] encode(TransactionOutput transactionOutput) throws Exception {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = null;
-        objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
         objectOutputStream.writeObject(transactionOutput);
         byte[] bytesTransactionOutput = byteArrayOutputStream.toByteArray();
         return bytesTransactionOutput;
@@ -45,16 +41,14 @@ public class EncodeDecodeUtil {
 
     public static TransactionOutput decodeToTransactionOutput(byte[] bytesTransactionOutput) throws Exception {
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytesTransactionOutput);
-        ObjectInputStream objectInputStream = null;
-        objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
         TransactionOutput transactionOutput = (TransactionOutput) objectInputStream.readObject();
         return transactionOutput;
     }
 
     public static byte[] encode(Block block) throws Exception {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = null;
-        objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
         objectOutputStream.writeObject(block);
         byte[] bytesBlock = byteArrayOutputStream.toByteArray();
         return bytesBlock;
@@ -62,26 +56,8 @@ public class EncodeDecodeUtil {
 
     public static Block decodeToBlock(byte[] bytesBlock) throws Exception{
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytesBlock);
-        ObjectInputStream objectInputStream = null;
-        objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
         Block block = (Block) objectInputStream.readObject();
         return block;
-    }
-
-    public static byte[] encode(List<TransactionOutput> transactionOutputList) throws Exception {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = null;
-        objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-        objectOutputStream.writeObject(transactionOutputList);
-        byte[] bytes = byteArrayOutputStream.toByteArray();
-        return bytes;
-    }
-
-    public static List<TransactionOutput> decodeToTransactionOutputList(byte[] byteTransactionOutputList) throws Exception {
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteTransactionOutputList);
-        ObjectInputStream objectInputStream = null;
-        objectInputStream = new ObjectInputStream(byteArrayInputStream);
-        List<TransactionOutput> transactionOutputList = (List<TransactionOutput>) objectInputStream.readObject();
-        return transactionOutputList;
     }
 }
