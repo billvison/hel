ba623a2d66df13220dda284eaff64cec4372deb3
邢开春 <xingkaichun@qq.com>
2020-05-28 19:05:31 +0800

    优化注释

--- helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/BlockChainCore.java
+++ helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/BlockChainCore.java
@@ -1,11 +1,11 @@
 package com.xingkaichun.helloworldblockchain.core;
 
 /**
- * 区块链Core，代表一个完成的区块链核心系统。
+ * 区块链核心，代表一个完整的区块链核心系统。
  * 区块链核心系统，由以下几部分组成：
  * 区块链数据库：用于持久化本地区块链的数据
- * 矿工系统：可以收集交易，挖矿，将新挖取的矿放进区块链数据库
- * 区块链同步器系统：区块链是一个分布式的数据库，同步器可以同步其它节点的区块链数据。
+ * 矿工：可以收集交易，挖矿，将新挖取的矿放进区块链数据库
+ * 区块链同步器：区块链是一个分布式的数据库，同步器可以同步其它节点的区块链数据。
  *
  * @author 邢开春 xingkaichun@qq.com
  */
@@ -16,10 +16,10 @@ public abstract class BlockChainCore {
     //矿工
     protected Miner miner ;
     //区块链同步器
-    protected Synchronizer synchronizer;
+    protected Synchronizer synchronizer ;
 
     /**
-     * 启动。激活矿工、激活区块链同步器。
+     * 激活区块链核心。激活矿工、激活区块链同步器。
      */
     public abstract void start();
 
