3616add316a8288d1d8c05c5d1d5e68d04dd94ac
邢开春 <xingkaichun@ceair.com>
2020-05-30 18:11:12 +0800

    删除TODO

--- helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/model/transaction/Transaction.java
+++ helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/model/transaction/Transaction.java
@@ -11,8 +11,6 @@ import java.util.List;
  * @author 邢开春 xingkaichun@qq.com
  */
 public class Transaction implements Serializable {
-    //TODO 无法固定交易的整体内容
-    //TODO BUG 交易输出可以作弊
 
     //交易时间戳
     private long timestamp;
