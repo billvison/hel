e19857f04f03c1f31e77a21288d74da1c4a05b8e
邢开春 <xingkaichun@ceair.com>
2020-11-28 17:30:39 +0800

    202011280002

--- helloworld-blockchain-explorer/src/main/java/com/xingkaichun/helloworldblockchain/explorer/HelloWorldBlockchainExplorerApplication.java
+++ helloworld-blockchain-explorer/src/main/java/com/xingkaichun/helloworldblockchain/explorer/HelloWorldBlockchainExplorerApplication.java
@@ -11,8 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 启动入口
- * //TODO 区块链浏览器项目拆分到独立的项目？？？还是在代码里标清楚浏览器部分的代码？？？
+ * 区块链浏览器
  * @author 邢开春 微信HelloworldBlockchain 邮箱xingkaichun@qq.com
  */
 @Configuration
