c007fc83c4427baf2f3c4033992d238c547ed5c4
邢开春 <xingkaichun@ceair.com>
2021-01-21 16:12:11 +0800

    2021012102

--- helloworld-blockchain-explorer/src/main/resources/static/js/account.js
+++ helloworld-blockchain-explorer/src/main/resources/static/js/account.js
@@ -15,7 +15,7 @@ function generateAccount(){
 	    error: function (e) {
 	    }
 	});
-	var getContent = '<h2 class="red">提示: 请在您信任的站点生成账户！请您保存好自己的账户!网站不会保存您的账户信息，且不对您的账户丢失负任何责任。</h2>' +
+	var getContent = '<h2 class="red">提示: 请在您信任的站点生成账户！</h2>' +
 					 '<div><span>账户(区块链领域学名:地址)</span>&nbsp;&nbsp;'+account.address+'</div>' +
 					 '<div><span>密码(区块链领域学名:私钥)</span>&nbsp;&nbsp;'+account.privateKey+'</div>' ;
 
