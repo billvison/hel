471028e29c6986c67974b417ab9be6055e593bdf
邢开春 <xingkaichun@qq.com>
2021-02-01 13:43:20 +0800

    20210201006

--- helloworld-blockchain-explorer/src/main/java/com/xingkaichun/helloworldblockchain/explorer/HelloWorldBlockchainExplorerApplication.java
+++ helloworld-blockchain-explorer/src/main/java/com/xingkaichun/helloworldblockchain/explorer/HelloWorldBlockchainExplorerApplication.java
@@ -2,16 +2,12 @@ package com.xingkaichun.helloworldblockchain.explorer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * 区块链浏览器
  * @author 邢开春 微信HelloworldBlockchain 邮箱xingkaichun@qq.com
  */
-@Configuration
 @SpringBootApplication
-@ServletComponentScan
 public class HelloWorldBlockchainExplorerApplication {
 
 	public static void main(String[] args) {
