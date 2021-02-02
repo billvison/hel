040d22b2d3d4165c60b675872773a98141a996a3
邢开春 <xingkaichun@qq.com>
2021-02-02 09:19:54 +0800

    2021020203

--- helloworld-blockchain-explorer/src/main/java/com/xingkaichun/helloworldblockchain/explorer/service/BlockchainBrowserService.java
+++ helloworld-blockchain-explorer/src/main/java/com/xingkaichun/helloworldblockchain/explorer/service/BlockchainBrowserService.java
@@ -1,10 +1,10 @@
 package com.xingkaichun.helloworldblockchain.explorer.service;
 
 import com.xingkaichun.helloworldblockchain.core.model.transaction.TransactionOutputId;
-import com.xingkaichun.helloworldblockchain.explorer.dto.transaction.TransactionOutputDetailView;
-import com.xingkaichun.helloworldblockchain.explorer.dto.transaction.TransactionView;
 import com.xingkaichun.helloworldblockchain.explorer.dto.transaction.SubmitTransactionToBlockchainNetworkRequest;
 import com.xingkaichun.helloworldblockchain.explorer.dto.transaction.SubmitTransactionToBlockchainNetworkResponse;
+import com.xingkaichun.helloworldblockchain.explorer.dto.transaction.TransactionOutputDetailView;
+import com.xingkaichun.helloworldblockchain.explorer.dto.transaction.TransactionView;
 
 import java.util.List;
 
@@ -15,13 +15,36 @@ import java.util.List;
  */
 public interface BlockchainBrowserService {
 
+    /**
+     * 根据交易输出ID获取交易输出
+     */
     TransactionOutputDetailView queryTransactionOutputByTransactionOutputId(TransactionOutputId transactionOutputId);
+    /**
+     * 根据地址获取交易输出
+     */
     List<TransactionOutputDetailView> queryTransactionOutputListByAddress(String address, long from, long size);
+    /**
+     * 根据地址获取未花费交易输出
+     */
     List<TransactionOutputDetailView> queryUnspendTransactionOutputListByAddress(String address, long from, long size);
+    /**
+     * 根据地址获取已花费交易输出
+     */
     List<TransactionOutputDetailView> querySpendTransactionOutputListByAddress(String address, long from, long size);
+    /**
+     * 根据交易哈希查询交易
+     */
     TransactionView queryTransactionByTransactionHash(String transactionHash);
+    /**
+     * 根据地址查询交易列表
+     */
     List<TransactionView> queryTransactionListByAddress(String address, long from, long size);
+    /**
+     * 根据区块哈希与交易高度查询交易列表
+     */
     List<TransactionView> queryTransactionListByBlockHashTransactionHeight(String blockHash, long from, long size);
-
+    /**
+     * 提交交易到区块链网络
+     */
     SubmitTransactionToBlockchainNetworkResponse submitTransactionToBlockchainNetwork(SubmitTransactionToBlockchainNetworkRequest request);
 }
