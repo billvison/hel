fad7e66981c3772ad9aa3ab53c220d502186d17b
邢开春 <xingkaichun@ceair.com>
2021-02-03 22:22:02 +0800

    202102030002

---
+++ helloworld-blockchain-explorer/src/main/resources/static/v3/assets/common/common.js
@@ -0,0 +1,21 @@
+async function $ajax(option){
+    const obj = {
+        type: "post",
+        contentType: "application/json",
+        dataType: "json",
+        data: `{}`,
+        async: false,
+    }
+    Object.assign(obj , option)
+    return new Promise ((resolve, rejact) => {
+        $.ajax({
+            ...obj,
+            success(data){
+                resolve(data)
+            },
+            error(err){
+                rejact(err)
+            }
+        })
+    })
+};
